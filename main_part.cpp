cb0f13b04b7891d908b4287b6fa28e2464af88a7
He-Who-Program-Things <109017378+THEWAZARUDO@users.noreply.github.com>
2024-08-02 23:08:53 +0700

    Update main_part.cpp

--- main_part.cpp
+++ main_part.cpp
@@ -1,20 +1,25 @@
 #include "Servo.h"
 
-Servo normalServo;
-Servo continuousServo;
 
-int pos = 0;
+Servo normalServo, normalServo2, continuousServo, continuousServo2; // khởi tạo các servo. normal: servo 180; continuous: servo 360.
 
 void setup()
 {
-    // khởi tạo servo, gắn vào pin 3;
-  	myservo.attach(3);
+    // khởi tạo pin cho các servo: 3;
+  	normalServo.attach(3);
+	normalServo2.attach(4);
+	continuousServo.attach(2);
+	continuousServo2.attach(6);
     // khởi tạo mode cho các pin 10, 11, 5, 9
- 	  pinMode(10, OUTPUT);
+ 	pinMode(10, OUTPUT);
   	pinMode(11, OUTPUT);
   	pinMode(5, OUTPUT);
   	pinMode(9, OUTPUT);
   	Serial.begin(9600);
+}
+
+void detech(){
+	
 }
 
 void operate(int motor_1_vel, int motor_2_vel, char state) // motor_1_vel: chỉ số chu kỳ xung của motor 1
@@ -24,14 +29,14 @@ void operate(int motor_1_vel, int motor_2_vel, char state) // motor_1_vel: chỉ
   	if(state == 'F'){
   		digitalWrite(5, LOW);
   		digitalWrite(9, HIGH);
-      digitalWrite(10, HIGH);
+      		digitalWrite(10, HIGH);
   		digitalWrite(11, LOW);
   	}
   	
   	else if (state == 'B'){
-      digitalWrite(5, HIGH);
-      digitalWrite(9, LOW);
-      digitalWrite(10, LOW);
+	      	digitalWrite(5, HIGH);
+	      	digitalWrite(9, LOW);
+	      	digitalWrite(10, LOW);
   		digitalWrite(11, HIGH);
     }
   	// motor 1: (+) - 10; (-) - 11
@@ -39,7 +44,7 @@ void operate(int motor_1_vel, int motor_2_vel, char state) // motor_1_vel: chỉ
   	// điều khiển 2 cực chéo nhau -> RPM xấp xỉ nhau
   	// 11 - 5 : ~700 - ~900 RPM
   	// 10 - 9 : ~1600 - ~1800 RPM
-	  analogWrite(11, motor_1_vel);
+	analogWrite(11, motor_1_vel);
   	analogWrite(5, motor_2_vel);
   	
   	Serial.println(motor_1_vel);
@@ -49,19 +54,20 @@ void operate(int motor_1_vel, int motor_2_vel, char state) // motor_1_vel: chỉ
 
 void loop(){
   	float vel; vel *= 255;
-	  operate(127, 127, 'B');
+	operate(127, 127, 'B');
 
-    continuousServo.write(180); // 90: Dừng servo, 0 - 89: quay servo ngược chiều kđh với tốc độ tỉ lệ nghịch với giá trị, 91 - 180: quay servo cùng chiều kđh với tốc độ tỉ lệ thuận với giá trị
-  
-    for(pos = 0; pos <= 180; pos++){
+    	continuousServo1.write(90); // 90: Dừng servo, 0 - 89: quay servo ngược chiều kđh với tốc độ tỉ lệ nghịch với giá trị, 91 - 180: quay servo cùng chiều kđh với tốc độ tỉ lệ thuận với giá trị
+  	continuousServo2.write(90);
+	
+    	for(pos = 0; pos <= 180; pos++){
   		normalServo.write(pos);
-    	delay(15);
+    		delay(15);
   	}
   	for(pos = 180; pos >= 0; pos--){
   		normalServo.write(pos);
-    	delay(15);
-        
+    		delay(15);
  	}
+	return 0;
 }
 /*
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⢿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
