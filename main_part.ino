fda9f76a02428a605428c8339941357cc3419da2
He-Who-Program-Things <109017378+THEWAZARUDO@users.noreply.github.com>
2024-08-05 02:56:55 -0700

    Update main_part.ino
    thêm hàm lọc dữ liệu đầu vaof

--- main_part.ino
+++ main_part.ino
@@ -9,6 +9,8 @@
 #define PS2_CLK 14 // SLK
 #define pressures false
 #define rumble false
+#define NUM_SAMPLE 10 // khởi tạo số mẫu thử để lọc dữ liệu
+
 
 //Khởi tạo class của thư viện
 PS2X ps2x; // khởi tạo class PS2x
@@ -24,12 +26,15 @@ const float WHITE_THRESHOLD = 255.0; // Ngưỡng độ sáng (c)
 const int trigPin = IO32;
 const int echoPin = IO2;
 int error = 0;
-
 void setup(){
 	//Khởi tạo Serial monitor với tốc độ 115200
 	Serial.begin(115200);
 	  
 	delay(300);  //thêm khoảng thời gian dừng để ổn định tay cầm
+
+	pwm.begin(); //khởi tạo PCA9685 
+	pwm.setOscillatorFrequency(27000000); // cài đặt tần số dao động 
+	pwm.setPWMFreq(50);// cài đặt tần số PWM.
 	
 	//khởi tạo config_game
 	for(int i = 0; i <= 9; ++i{
@@ -92,12 +97,38 @@ void setup(){
 	normalServo2.write(90);
 }
 
+
 void detech(){
-	uint16_t r, g, b, c;
+	uint16_t r, g, b, c, distance;
+	uint16_t r_sum = 0, g_sum = 0, b_sum = 0, c_sum = 0; //tổng giá trị của mỗi lần đo
 
-	// Đọc giá trị màu sắc từ cảm biến
-	tcs.getRawData(&r, &g, &b, &c);
-
+	//lọc dữ liệu đầu vào từ 
+	for(int i = 0; i <= NUM_SAMPLES; ++i){
+		// Đọc giá trị màu sắc từ cảm biến
+		tcs.getRawData(&r, &g, &b, &c);
+		r_sum += r; //lưu trữ giá trị của mỗi lần đo, chia lấy trung bình và từ đó hiệu chỉnh giá trị đầu vào và giảm sai số 
+		g_sum += g;
+		b_sum += b;
+		c_sum += c;
+		
+		// Đảm bảo chân Trig ở mức LOW để bắt đầu một tín hiệu mới
+		digitalWrite(trigPin, LOW);
+		delayMicroseconds(2);
+		// Kích hoạt chân Trig trong 10 micro giây
+		digitalWrite(trigPin, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(trigPin, LOW);
+		
+		// Đọc thời gian tín hiệu Echo ở mức HIGH
+		d_sum += pulseIn(echoPin, HIGH);
+		
+	}
+	// chia tổng giá trị nhận được với só lần thử để nhận được giá trị trung bình, hay là giá trị đầu vào đã lọc
+	r = r_sum / NUM_SAMPLE;
+	g = g_sum / NUM_SAMPLE;
+	b = b_sum / NUM_SAMPLE;
+	c = c_sum / NUM_SAMPLE;
+	duration = d_sum / NUM_SAMPLE;
 	// Kiểm tra xem màu nhận được có phải là màu trắng hay không
 	if (c > WHITE_THRESHOLD){ //nếu màu clear nhận được lớn hơn 255 => màu trắng
 		continuousServo1.write(180);
@@ -109,28 +140,12 @@ void detech(){
 		delay(1000);
 		continuousServo2.write(90);
   	}
-	long duration;
-	int distance;
-	
-	// Đảm bảo chân Trig ở mức LOW để bắt đầu một tín hiệu mới
-	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2);
-	
-	// Kích hoạt chân Trig trong 10 micro giây
-	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
-	
-	// Đọc thời gian tín hiệu Echo ở mức HIGH
-	duration = pulseIn(echoPin, HIGH);
-	// Tính khoảng cách theo công thức
-	distance = duration * 0.034 / 2;
-	
+	distance = duration * 0.034 / 2; //(vì xung phát mỗi 10s)
 	// In khoảng cách ra Serial Monitor
 	Serial.print("Distance: ");
 	Serial.print(distance);
 	Serial.println(" cm");
-	if(distance == 15 && LWheel_vel > 0 && RWheel_vel > 0)
+	if(distance <= 15 && LWheel_vel > 0 && RWheel_vel > 0)
 	{
 		digitalWrite(10, LOW);
 		digitalWrite(11, LOW);
@@ -146,37 +161,35 @@ void control(int LWheel_vel,  int RWheel_vel) // state: trạng thái di chuyể
 {
 	// kiểm tra liệu chỉ số chu kỳ xung có âm không, nếu có thì đảo chiều động cơ và đổi dấu 
   	if(LWheel_vel > 0){
-		//đặt 2 motor xoay ngược chiều nhau => robot đi thẳng (vì 2 motor đối diện nhau)
+		//đặt motor 1 quay ngược kim đồng hồ
   		digitalWrite(12, LOW);
-  		digitalWrite(13, HIGH); 
+  		digitalWrite(13, HIGH);
 	}
 	else if(LWheel_vel < 0)
 	{
-		//đặt ngược chiều quay của motor để khiến motor chạy lùi
+		//đặt motor 1 quay theo chiều kim đồng hồ
 		digitalWrite(12, HIGH);
 		digitalWrite(13, LOW);
 		LWheel_vel *= -1;
 	}
 	if(RWheel_vel > 0){
-		
+		//đặt motor 2 quay thuận chiều kim đồng hồ
       		digitalWrite(10, HIGH);
   		digitalWrite(11, LOW);
   	}
-	else if(RWheel_vel < 0)
-	{
+	else if(RWheel_vel < 0){
+		//đặt motor 2 quay ngược chiều kim đồng hồ
 	      	digitalWrite(10, LOW);	  		
 		digitalWrite(11, HIGH);
 		RWheel_vel *= -1;
 	}
   	// motor 1: (+) - 10; (-) - 11
   	// motor 2: (+) - 12 ; (-) - 13
-  	// điều khiển 2 cực chéo nhau -> RPM xấp xỉ nhau
-  	// 11 - 12 : ~700 - ~1300 RPM
-  	// 10 - 13 : ~1600 - ~1800 RPM
+  	// điều khiển 2 cực chéo -> RPM xấp xỉ nhau (dựa trên mô phỏng ở tinkercad.com)
 
 	//điều khiển tốc độ của 2 bánh bằng cách băm xung PWM (đúng, tôi vừa ghi là xung xung điều chỉnh độ rộng đấy)
-	analogWrite(11, LWheel_vel);
-  	analogWrite(12, RWheel_vel);
+	pwm.setpwm(11, LWheel_vel);
+  	pwm.setpwm(12, RWheel_vel);
 
   	Serial.println(LWheel_vel);
   	Serial.println(RWheel_vel);
@@ -200,7 +213,7 @@ void loop(){
 	
 	if(ps2x.Button(PSB_SQUARE) && !ps2x.button(PSB_TRIANGLE))//nếu nút vuông đang được nhấn và nút tam giác không được nhấn thì
 	{
-		digitalWrite(14, LOW); //chạy ròng rọc theo chiều kim đồng hồ = hạ ròng rọc
+		digitalWrite(14, LOW); //chạy ròng rọc ngược chiều kim đồng hồ = hạ ròng rọc
   		digitalWrite(15, HIGH); 
 	}
 	else if(!ps2x.Button(PSB_SQUARE)){ // nếu không thì
